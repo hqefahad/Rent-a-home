9af7cb191c01fa52bd0252c5b20abdbee0843438
Fahad Haque <Fahadhaque14@gmail.com>
2021-08-31 21:09:48 +0600

    Initial commit

---
+++ javascript/index.js
@@ -0,0 +1,39 @@
+document.querySelector(".btn").addEventListener("click",buttonClicked)
+
+function buttonClicked(){
+    alert("This button is in under construction")
+}
+
+$('.responsive').slick({
+    dots: true,
+    speed: 300,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 1,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+  });
